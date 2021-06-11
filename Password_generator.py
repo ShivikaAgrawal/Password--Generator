1c6f102877afa1ad6cb60fbb56aa4b1a873871c2
ShivikaAgrawal <74872250+ShivikaAgrawal@users.noreply.github.com>
2021-06-11 23:19:15 +0530

    Add files via upload
    Suugestion Required ? Program i$ Here!!

---
+++ Password_generator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Suggestion of strong Password
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+sg_letters= int(input("How many letters would you like in your password?\n")) 
+sg_symbols = int(input(f"How many symbols would you like?\n"))
+sg_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = []
+for char in range(1, sg_letters+1):
+  password+=random.choice(letters)
+for char in range(1, sg_symbols+1):
+  password+=random.choice(symbols)
+for char in range(1, sg_numbers+1):
+  password+=random.choice(numbers)
+
+random.shuffle(password)
+pass1=""
+for char in password:
+  pass1+= char
+
+print("Your suggestion is : ", pass1)
+
